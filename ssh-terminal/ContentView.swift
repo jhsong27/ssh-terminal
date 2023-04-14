d2659685b068e59770620ed0baee44259975f598
Jaehyun Song <songjh0207@gmail.com>
2023-04-14 18:43:03 +0900

    v0.01

--- ssh-terminal/ContentView.swift
+++ ssh-terminal/ContentView.swift
@@ -15,7 +15,6 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, World!")
             Text("1st Line!")
-            Text("2nd Line!")
         }
         .padding()
     }
