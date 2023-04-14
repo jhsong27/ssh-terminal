4c39e6f8b081425da394815b49eed9aedbe3e4f9
Jaehyun Song <songjh0207@gmail.com>
2023-04-14 17:27:25 +0900

    Initial Commit

---
+++ ssh-terminal/ssh_terminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ssh_terminalApp.swift
+//  ssh-terminal
+//
+//  Created by Jaehyun Song on 2023/04/14.
+//
+
+import SwiftUI
+
+@main
+struct ssh_terminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
